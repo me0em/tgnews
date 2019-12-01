ddae43d8c41754ccfcdda75e9bb097d9775a0d44
me0em <38053198+me0em@users.noreply.github.com>
2019-12-01 05:35:35 +0300

    Sets and operations with them
    Things for SimilarityMeasure()

--- tools/tools.go
+++ tools/tools.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 	"strings"
 	"encoding/json"
+	"math"
 )
 
 // Abs for Out-of-Place measure
@@ -207,19 +208,26 @@ func BagOfWordsOverFiles(filePaths []string, top int) frequency {
 	freq.HashTable = make(map[string]int)
 
 	for _, filePath := range filePaths {
+		tmpMap := make(map[string]int)
 		htmlData, _ := ioutil.ReadFile(filePath)
 		words := Dvornik(string(htmlData))
 
 		for _, word := range words {
-			if _, isKeyExists := freq.HashTable[word]; isKeyExists {
-				freq.HashTable[word] += 1
+			if _, isKeyExists := tmpMap[word]; isKeyExists {
+				tmpMap[word] += 1
 			} else {
-				freq.HashTable[word] = 1
+				tmpMap[word] = 1
+			}
+		}
+
+		for key,_ := range(tmpMap) {
+			if _, isKeyExists := freq.HashTable[key]; isKeyExists {
+				freq.HashTable[key] += 1
+			} else {
+				freq.HashTable[key] = 1
 			}
 		}
 	}
-
-	freq.Top = MapToSortedCuttedArray(freq.HashTable, top)
 	return freq
 }
 
@@ -252,4 +260,108 @@ func MapToSortedCuttedArray(someMap map[string]int, top int) []string {
 		}
 	}
 	return topArray
+}
+
+// en.wikipedia.org/wiki/Tf%E2%80%93idf
+func TFIDF(textFreq frequency, corpusBagOfWords map[string]int, corpusLength int) map[string]float {
+	result := make(map[string]float)
+	topAmount := 0
+	for _,word := range(textFreq.Top) {
+		topAmount += textFreq.HashTable[word]
+	}
+
+	for for _,word := range(textFreq.Top) {
+		data[word] = textFreq.HashTable[word] / topAmount * math.Log(corpusLength / corpusBagOfWords[word])
+	}
+	return data
+}
+
+//
+// Sets and operations with them
+type void struct{}
+
+type Set struct {
+	Data map[float]void // empty set
+}
+
+func (s Set) Add(value float) {
+	var illusion void
+	s.Data[value] = illusion
+}
+
+func (s Set) Delete(value float) {
+	delete(s.Data, value)
+}
+
+func (s Set) Size() int {
+	return len(s.Data)
+}
+
+func (s Set) IsExists(value float) bool {
+	_, result := s.Data[value]
+	return result
+}
+
+func (a Set) Union(b Set) Set {
+	c := a
+	for value := range b.Data {
+    if c.IsExists(value) == false {
+			c.Add(value)
+		}
+	}
+	return c
+}
+
+func (a Set) Intersection(b Set) Set {
+	var c Set
+	for value := range b.Data {
+		if a.IsExists(value) {
+			c.Add(value)
+		}
+	}
+	return c
+}
+
+func (a Set) SymmetricDifference(b Set) Set {
+var c Set
+	for value := range b.Data {
+		if a.IsExists(value) {
+			c.Add(value)
+		}
+	}
+
+	for value := range a.Data {
+		if b.IsExists(value) {
+			c.Add(value)
+		}
+	}
+	return c
+}
+
+func SimilarityMeasure(frequencyA, frequencyB frequency) float {
+	var (
+		a Set
+		b Set
+	)
+
+	for _,v := range(frequencyA.Top) {
+		a.Add(v)
+	}
+	for _,v := range(frequencyB.Top) {
+		b.Add(v)
+	}
+	c := a.Intersection(b)
+
+	intersectionSum := 0
+	for _,v := range(c) {
+		intersectionSum += math.min(frequencyA.HashTable[v], frequencyB.HashTable[v])
+	}
+
+	c = a.Union(b)
+	unionSum := 0
+	for _,v := range(c) {
+		 unionSum += frequencyA.HashTable[v] + frequencyB.HashTable[v]
+	}
+
+	return intersectionSum / (unionSum - intersectionSum)
 }
